692daea40e36aa94a3c7a501f24c007349c6bb8c
Viniciuss.bezerra@outlook.com <viniciuss.bezerra@outlook.com>
2022-01-30 15:25:15 -0300

    adicionando menu para dispositivos moveis

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+
+function menuHamburguer(){
+
+    const menuMobile = document.querySelector("div.menu-mobile")
+
+    const nav = document.querySelector("nav")
+
+    nav.classList.toggle('active')
+
+    console.log("ola")
+    
+}
+
+
+
+
