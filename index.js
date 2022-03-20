90688e70bbb8402b41161b0b2b72d32caea69ed2
vinicius <viniciussouza2599@gmail.com>
2022-03-20 17:13:46 -0300

    Ajustando layout e adicionando uma nova tecnologia ao meu portifolio

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 
-
-
 function menuHamburguer(){
-
-    const menuMobile = document.querySelector("div.menu-mobile")
 
     const nav = document.querySelector("nav")
 
     nav.classList.toggle('active')
-
-    console.log("ola")
-    
-}
+};
 
 
 
