6a53cb7bab9d5868caf21c2b63390612913663f6
vinicius souza <viniciussouza2599@gmail.com>
2022-04-28 15:02:57 -0300

    refatorando o codigo do portifolio

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,16 @@
 
 
+function toogleMenu() {
+    const toogleMenu = document.querySelector('nav');
+    toogleMenu.classList.toggle('active');
+}
 
+function renderPage() {
+    $('#header').load("./pages/Header/header.html");
+    $('#aboutme').load("./pages/aboutme/aboutme.html");
+    $('#technology').load("./pages/technology/technology.html");
+    $('#project').load("./pages/Projects/projects.html");
+    $('#footer').load("./pages/footer/footer.html");
+}
 
+renderPage();
