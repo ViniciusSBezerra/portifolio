02d4c202af2917a081aecaf815389ac0cda14b2f
vinicius souza <viniciussouza2599@gmail.com>
2022-04-28 12:16:21 -0300

    testando algumas coisas

--- scripts/script.js
+++ scripts/script.js
@@ -8,11 +8,5 @@ function toogleMenu(){
 }
 
 
-$(function () {
-    $('#header').load("../pages/Header/header.html");
-    $('#about-me').load("../pages/About-me/aboutMe.html");
-    $('#technology').load("../pages/technology/technology.html");
-    $('#project').load("../pages/Projects/projects.html");
-    $('#footer').load("../pages/Footer/footer.html");
-});
+
 
