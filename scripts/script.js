9c08c1027bdcf44ee3909a3d9da6683733518456
vinicius souza <viniciussouza2599@gmail.com>
2022-04-28 10:16:18 -0300

    refatorando o codigo

--- scripts/script.js
+++ scripts/script.js
@@ -9,10 +9,10 @@ function toogleMenu(){
 
 
 $(function () {
-    $('#header').load("./pages/Header/header.html");
-    $('#about-me').load("./pages/About-me/aboutMe.html");
-    $('#technology').load("./pages/technology/technology.html");
-    $('#project').load("./pages/Projects/projects.html");
-    $('#footer').load("./pages/Footer/footer.html");
+    $('#header').load("../pages/Header/header.html");
+    $('#about-me').load("../pages/About-me/aboutMe.html");
+    $('#technology').load("../pages/technology/technology.html");
+    $('#project').load("../pages/Projects/projects.html");
+    $('#footer').load("../pages/Footer/footer.html");
 });
 
