39912441caebb6e1ff0b91b65411eeec37396673
vinicius souza <viniciussouza2599@gmail.com>
2022-04-28 10:12:44 -0300

    refatorando o codigo

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,12 @@ function toogleMenu(){
 
 }
 
+
+$(function () {
+    $('#header').load("./pages/Header/header.html");
+    $('#about-me').load("./pages/About-me/aboutMe.html");
+    $('#technology').load("./pages/technology/technology.html");
+    $('#project').load("./pages/Projects/projects.html");
+    $('#footer').load("./pages/Footer/footer.html");
+});
+
