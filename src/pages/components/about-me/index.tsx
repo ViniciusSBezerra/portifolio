fde2c6d8192549d2b0dcc23e98468831bb29b19f
vinicius <viniciussouza2599@gmail.com>
2024-03-12 18:23:44 -0300

    update text

--- src/pages/components/about-me/index.tsx
+++ src/pages/components/about-me/index.tsx
@@ -9,13 +9,7 @@ export const AboutMe = () => {
     <div className="container__aboutMe">
       <h2 className={!theme ? "title" : "title dark-theme"}>Sobre mim</h2>
       <p className={!theme ? "aboutMe" : "aboutMe dark-theme"}>
-        Olá, sou Vinicius Souza, um desenvolvedor web de 23 anos, atualmente
-        empregado na QualityDigital, onde tenho a emocionante oportunidade de
-        contribuir para projetos de destaque, incluindo Electrolux, Multilaser,
-        Sauer e Obramax. Essas empresas líderes são parte integrante do nosso
-        portfólio de projetos na QualityDigital, onde aplico minhas habilidades
-        e conhecimentos no aprimoramento da experiência do usuário e no
-        desenvolvimento de soluções inovadoras.
+        Olá! Sou Vinicius Souza, um desenvolvedor web especializado em projetos de destaque para marcas líderes como Electrolux, Multilaser, Livedros, Osklen, Sauer e Obramax. Com minhas habilidades e conhecimentos, aprimoro a experiência do usuário e crio soluções inovadoras. Se você procura excelência no desenvolvimento web, estou aqui para levar seu projeto ao próximo nível. Entre em contato e vamos fazer acontecer!
       </p>
     </div>
   );
