b1d344bb786cddda6f0a913eb1d6e5da5790cd68
= <=>
2024-09-02 19:45:48 -0300

    feat: adjusting footer

--- src/pages/components/footer/index.tsx
+++ src/pages/components/footer/index.tsx
@@ -4,7 +4,7 @@ import { SocialMedia } from "../social-media";
 export const Footer = () => {
   return (
     <footer className="h-24 mx-auto relative w-full bottom-0  flex items-center justify-center mt-8 shadow-top-lg">
-      <SocialMedia />
+      <SocialMedia isFooter={true} />
     </footer>
   );
 };
--- src/pages/components/social-media/index.tsx
+++ src/pages/components/social-media/index.tsx
@@ -3,9 +3,13 @@ import { InstagramIcon } from "../../../assets/InstagramIcon";
 
 import { LikedinIcon } from "../../../assets/LinkedinIcon";
 
-export const SocialMedia = () => {
+interface SocialMediaProps {
+    isFooter?: boolean
+}
+
+export const SocialMedia = ({isFooter}: SocialMediaProps) => {
     return (
-        <div className="flex gap-6 justify-between mt-6">
+        <div className={` flex gap-6 justify-between ${isFooter ? "mt0": "mt6"}`}>
             <a target="_blank" aria-label="link github" rel="noopener" href="https://github.com/ViniciusSBezerra">
                 <GitHubIcon />
             </a>
