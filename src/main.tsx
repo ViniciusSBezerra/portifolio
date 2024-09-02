fd3efcfc1edd2190eb486c17faec8576ad15a18c
= <=>
2024-09-02 19:27:08 -0300

    feat: remove component not used

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
-import { Suspense } from "react";
+
 import ReactDOM from "react-dom/client";
 import { GlobalProvider } from "./context/index.tsx";
 import "./app.css";
-import Loading from "./pages/components/Loading/Loading.tsx";
 import { App } from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Suspense fallback={<Loading />}>
+
     <GlobalProvider>
       <App />
     </GlobalProvider>
-  </Suspense>
-
+  
 );
